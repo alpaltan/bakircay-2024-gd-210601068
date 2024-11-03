5960dd9027d34f8f23f893dfc366d0e784f1cd27
Seref <seref.filizcan@gmail.com>
2024-11-04 00:44:36 +0300

    feat: touch manager, prefabs, trigger detection implemented

--- Assets/Scripts/PhysicsExample/DetectObject.cs
+++ Assets/Scripts/PhysicsExample/DetectObject.cs
@@ -1,5 +1,6 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using Common;
 using UnityEngine;
 
 public class DetectObject : MonoBehaviour
@@ -8,27 +9,60 @@ public class DetectObject : MonoBehaviour
     private Camera _mainCamera;
 
     private Rigidbody _rigidbodyToJump = null;
+
+    private Vector3 _totalForce = Vector3.zero;
+    private float _forceFactor = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
         _mainCamera = Camera.main;
+
+        TouchManager.Instance.OnTouchBegan += OnTouchBegan;
+        TouchManager.Instance.OnTouchMoved += OnTouchMoved;
+        TouchManager.Instance.OnTouchEnded += OnTouchEnded;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnDestroy()
     {
-        //Raycast on screen touch
-        if (Input.GetMouseButtonDown(0))
+        if (TouchManager.Instance == null)
+            return;
+
+        TouchManager.Instance.OnTouchBegan -= OnTouchBegan;
+        TouchManager.Instance.OnTouchMoved -= OnTouchMoved;
+    }
+
+    private void OnTouchBegan(TouchData data)
+    {
+        CastRay(data.position);
+    }
+
+
+    private void OnTouchMoved(TouchData data)
+    {
+        if (_rigidbodyToJump != null)
         {
-            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
+            _totalForce += new Vector3(data.deltaPosition.x, 1f, data.deltaPosition.y) * Time.deltaTime;
+        }
+    }
+
+    private void OnTouchEnded(TouchData data)
+    {
+        _rigidbodyToJump = null;
+    }
+
+
+    private void CastRay(Vector2 screenPosition)
+    {
+        Ray ray = _mainCamera.ScreenPointToRay(screenPosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("Object name: " + hit.transform.name);
+            if (hit.rigidbody != null)
             {
-                Debug.Log("Object name: " + hit.transform.name);
-                if (hit.rigidbody != null)
-                {
-                    _rigidbodyToJump = hit.rigidbody;
-                }
+                _rigidbodyToJump = hit.rigidbody;
             }
         }
     }
@@ -37,16 +71,14 @@ public class DetectObject : MonoBehaviour
     {
         if (_rigidbodyToJump != null)
         {
-            Debug.Log("Force before :" + _rigidbodyToJump.velocity);
-            _rigidbodyToJump.AddForce(Vector3.up * 10f, ForceMode.Impulse);
-            StartCoroutine(RigidbodyVelocity(_rigidbodyToJump));
-
-            _rigidbodyToJump = null;
+            // Debug.Log("Force before :" + _rigidbodyToJump.velocity);
+            _rigidbodyToJump.AddForce(_totalForce * _forceFactor, ForceMode.Impulse);
+            _totalForce = Vector3.zero;
         }
 
     }
 
-    private IEnumerator RigidbodyVelocity(Rigidbody rb)
+    /* private IEnumerator RigidbodyVelocity(Rigidbody rb)
     {
         yield return new WaitForFixedUpdate();
 
@@ -56,14 +88,6 @@ public class DetectObject : MonoBehaviour
             yield return new WaitForSeconds(0.1f);
             yield return new WaitForFixedUpdate();
         }
-    }
+    }   */
 
-    private void OnDrawGizmos()
-    {
-        //draw ray 
-        if (_mainCamera == null)
-            return;
-        Ray ray = _mainCamera.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f));
-        Gizmos.DrawRay(ray);
-    }
 }
